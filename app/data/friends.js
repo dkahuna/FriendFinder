9605391b99d3a25b69230a6c88bb1b07b7447585
Khamhuang, David <khameye19888@gmail.com>
2019-08-03 20:51:52 -0500

    added photos to friends list

--- app/data/friends.js
+++ app/data/friends.js
@@ -21,8 +21,8 @@ var friends = [
   },
 
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Monica",
+    "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fvignette.wikia.nocookie.net%2Ffriends%2Fimages%2F2%2F2f%2FMonica_Geller-Bing_Season_10.png%2Frevision%2Flatest%3Fcb%3D20180426182234&imgrefurl=https%3A%2F%2Ffriends.fandom.com%2Fwiki%2FMonica_Geller&docid=AllGQ6mGT-WjeM&tbnid=bceF0m3kBa-BXM%3A&vet=10ahUKEwjU4Knf7eLjAhVBjlkKHdL1ARYQMwhrKAMwAw..i&w=618&h=836&bih=792&biw=1536&q=friends%20cast&ved=0ahUKEwjU4Knf7eLjAhVBjlkKHdL1ARYQMwhrKAMwAw&iact=mrc&uact=8",
     "scores":[
         "2",
         "5",
@@ -38,8 +38,8 @@ var friends = [
   },
 
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Joey",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwijluua6ufjAhXNY98KHcQBAmwQjRx6BAgBEAU&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fen.wikipedia.org%252Fwiki%252FJoey_Tribbiani%26psig%3DAOvVaw0Kyq-bN94uvw2t7RzeEV9l%26ust%3D1564960349728036&psig=AOvVaw0Kyq-bN94uvw2t7RzeEV9l&ust=1564960349728036",
     "scores":[
         "3",
         "2",
@@ -55,8 +55,8 @@ var friends = [
   },
 
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Rachel",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj0692I6-fjAhXFmOAKHXYDBVgQjRx6BAgBEAU&url=https%3A%2F%2Fjerseymeetscountry.wordpress.com%2F2013%2F10%2F21%2Ffriends-the-cast-that-never-was%2F&psig=AOvVaw02sts0DcEO8rcSp5072wSq&ust=1564960692497684",
     "scores":[
         "3",
         "3",
@@ -72,8 +72,8 @@ var friends = [
   },
 
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Chandler",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjA_NW86-fjAhWFMd8KHcLNBUAQjRx6BAgBEAU&url=https%3A%2F%2Fwww.instyle.com%2Ffashion%2Fthen-and-now-cast-friends-and-how-theyve-changed-20-years&psig=AOvVaw2NV0evm5H2LL-MMUtiVkpz&ust=1564960726107427",
     "scores":[
         "4",
         "1",
@@ -89,8 +89,8 @@ var friends = [
   },
 
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Phoebe",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjbnJHt6-fjAhXKMd8KHfS1BtQQjRx6BAgBEAU&url=https%3A%2F%2Fwww.instyle.com%2Ffashion%2Fthen-and-now-cast-friends-and-how-theyve-changed-20-years&psig=AOvVaw312Z6aR19LSXN4NmHSd2RY&ust=1564960903276166",
     "scores":[
         "5",
         "5",
@@ -106,8 +106,8 @@ var friends = [
   },
 
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Ursula",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjKx6er7-fjAhUuT98KHQ_LAEcQjRx6BAgBEAU&url=https%3A%2F%2Ffriends.fandom.com%2Fwiki%2FUrsula_Buffay&psig=AOvVaw0oFTqtt5BjsqZtRKOQlOnS&ust=1564960937651254",
     "scores":[
         "2",
         "5",
@@ -123,8 +123,8 @@ var friends = [
   },
 
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Ross",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjvl8eI8efjAhUwTt8KHadzC8kQjRx6BAgBEAU&url=https%3A%2F%2Fhome.adelphi.edu%2F~ji21632%2Fmeet%2520the%2520characters.html&psig=AOvVaw1Bs0kKNFhOnOuJQ8smI007&ust=1564962235045797",
     "scores":[
         "1",
         "5",
@@ -140,8 +140,8 @@ var friends = [
   },
 
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Janet",
+    "photo": "https://www.thesun.co.uk/wp-content/uploads/2017/06/nintchdbpict000328399773.jpg",
     "scores":[
         "2",
         "2",
@@ -158,8 +158,8 @@ var friends = [
   },
 
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Gunther",
+    "photo": "https://hips.hearstapps.com/cosmouk.cdnds.net/14/38/1600x800/nrm_1411034674-friends-gunther-secrets-janice-cosmopolitan.jpg?resize=1200:*",
     "scores":[
         "1",
         "1",
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,54 @@ module.exports = function (app) {
         res.json(friends);
     });
 
-    
+    app.post("/api/friends", function (req, res) {
 
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+
+        console.log(req.body);
+
+        // Here we take the result of the user's survey POST and parse it.
+        var userData = req.body;
+        var userScores = userData.scores;
+
+
+        // This variable will caculate the difference between the user's scores and the 
+        // scores of each user in the database
+        var totalDifference = 0;
+
+        // Here we loop through all of the friends possible in the database
+        for (var i = 0; i < friends.length; i++) {
+            
+            console.log(friends[i]);
+            totalDifference = 0;
+
+            // We then loop through all the scores of each friend
+            for (var j = 0; j < friends[i].scores[j]; j++) {
+
+                // We calculate the difference between the scores and sum them into the totalDifference
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+                // If the sum of the difference is less than the differences of the current "best match"
+                if (totalDifference <= bestMatch.friendDifference) {
+
+                    // Reset the bestMatch to be the new friend.
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.friendDifference = totalDifference;
+                }
+            }
+        }
+
+        // Finally save the user's data to the database (this has to happen AFTER the check. OTHERWISE,
+        // the database will always return that the user is the user's best friend).
+        friends.push(userData);
+
+        //Return a JSON with the user's bestMatch. This will be used by the HTML in the next page.
+        res.json(bestMatch);
+    });
+    
 }
